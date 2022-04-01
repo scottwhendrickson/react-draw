a0ecf5c52f54e6423d840192ba900813cb681aab
Scott Hendrickson <hesct@amazon.com>
2022-04-01 15:13:50 -0400

    Canvas creation update

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import CanvasDraw from "./RCD";
 import uuid from 'uuid/v4'
-
+//
 import { API, graphqlOperation } from 'aws-amplify'
 import { onUpdateCanvas } from './graphql/subscriptions'
 import { updateCanvas, createCanvas } from './graphql/mutations'
